d0089be1ce4eafcc006e86da6bee334ec23d6137
nick-meyer <nrmeyer08@gmail.com>
2022-08-11 15:12:44 -0700

    Update all.js

--- src/all.js
+++ src/all.js
@@ -1,7 +1,5 @@
 const https = require("https");
 
-setInterval(() => (https.get("https://all-bot-salmon.herokuapp.com"), 3*600000));
-
 // Bot configs read in from environment
 const room_id = process.env.HUBOT_GROUPME_ROOM_ID;
 const bot_id = process.env.HUBOT_GROUPME_BOT_ID;
